328c82e60aff2bd51d7b758cdad692b2b763493f
Perkles <davifernandosousadias18@gmail.com>
2024-03-02 20:09:25 -0300

    some product with paramentros

--- src/main.py
+++ src/main.py
@@ -9,6 +9,9 @@ def hello():
     "age":19,
     "logaduro": "travessa jose scarcela {247}"
     })
+@app.route('/soma/<int:value_one>/<int:value_two>/')    
+def soma(value_one,value_two):
+    return jsonify({"soma":value_one+value_two})
 
 if __name__ =="__main__":
     app.run(debug=True)
