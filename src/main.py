4594605f41e64e10a817a82b33889ad5cdfb501a
Perkles <davifernandosousadias18@gmail.com>
2024-03-02 19:33:05 -0300

    add jsonify

--- src/main.py
+++ src/main.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask,jsonify
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "heloo world"
+    return jsonify({
+    "name":"davi",
+    "age":19,
+    "logaduro": "travessa jose scarcela {247}"
+    })
 
 if __name__ =="__main__":
     app.run(debug=True)
