e817d9056ab37f6caca26aa4a1a2a0a343480fc2
Perkles <davifernandosousadias18@gmail.com>
2024-03-02 20:44:06 -0300

    init project

--- src/main.py
+++ src/main.py
@@ -1,17 +1,11 @@
 from flask import Flask,jsonify
 
-app = Flask(__name__)
+app = Flask=(__name__)
 
-@app.route("/")
-def hello():
-    return jsonify({
-    "name":"davi",
-    "age":19,
-    "logaduro": "travessa jose scarcela {247}"
-    })
-@app.route('/soma/<int:value_one>/<int:value_two>/')    
-def soma(value_one,value_two):
-    return jsonify({"soma":value_one+value_two})
+@app.route('/dev')
+def desenvolvedor():
+    return jsonify({'nome':'rafael'})
 
-if __name__ =="__main__":
-    app.run(debug=True)
+
+if __name__ == '__main__':
+    app.run()
