bf5e649125c090260de2f7bb7b168108c5b7b3f3
Perkles <davifernandosousadias18@gmail.com>
2024-03-05 19:05:34 -0300

    create api test

--- src/main.py
+++ src/main.py
@@ -1,11 +1,23 @@
 from flask import Flask,jsonify
 
-app = Flask=(__name__)
+app = Flask(__name__)
 
-@app.route('/dev')
-def desenvolvedor():
-    return jsonify({'nome':'rafael'})
+devolpers = [
+    {'name':'davi',
+    'hability':['Python','Django']
+    },
+    {'name':'Gabriel',
+    'hability':['Python','Django']
+    }
+]
+
+@app.route('/dev/<int:id>/')
+def devolper(id):
+    devolper=devolpers[id]
+    print(devolper)
+    return jsonify(devolper)
+
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
