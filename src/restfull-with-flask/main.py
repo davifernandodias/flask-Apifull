379a83b66202aeb1a935c715f85e6c1ff6e6b3e3
Perkles <davifernandosousadias18@gmail.com>
2024-03-05 22:13:46 -0300

    finalize api-flashrestfull

--- src/restfull-with-flask/main.py
+++ src/restfull-with-flask/main.py
@@ -1,6 +1,6 @@
-from flask import Flask
+from flask import Flask,request
 from flask_restful import Resource, Api
-
+import json
 app = Flask(__name__)
 api = Api(app)
 
@@ -27,12 +27,28 @@ class Devolper(Resource):
             mensagem = 'erro desconhecido. procuro adminstritador da api'
             response = {'status':'erro','mensagem': mensagem}
         return response
-    def put(self):
-        pass
-    def delete(self):
-        pass
+    def put(self,id):
+        dados = json.loads(request.data)
+        devolpers[id]=dados
+        return dados
+    def delete(self,id):
+        devolpers.pop(id)
+        return {'status': 'sucesso','mensagem': 'registro excluido'}
+
+
+class ListaDesenvolvedores(Resource):
+    def get(self):
+        return devolpers
+
+    def post(self):
+        dados=json.loads(request.data)
+        posicao = len(devolpers)
+        dados['id']= posicao
+        devolpers.append(dados)
+        return devolpers[posicao]
 
 api.add_resource(Devolper,'/dev/<int:id>/')
+api.add_resource(ListaDesenvolvedores,'/dev/')
 
 if __name__ == '__main__':
     app.run(debug=True)
